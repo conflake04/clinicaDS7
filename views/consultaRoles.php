cbd424f66d99a8ebff006f2cc49a7b0a79ae3caf
amirj <amirjhapargonzalezcamarena@gmail.com>
2024-10-28 16:03:07 -0500

    css 4

--- views/consultaRoles.php
+++ views/consultaRoles.php
@@ -1,34 +1,33 @@
 <?php require 'templates/header.php'; ?>
 
 <link rel="stylesheet" href="./css/consultaRoles.css">
-<body>
-    <div class="container">
+ 
 
-        <div class="table-container">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nombre del Rol</th>
-                        <th>Descripción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if (!empty($roles)): ?>
-                        <?php foreach ($roles as $rol): ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($rol['name_rol']); ?></td>
-                                <td><?php echo htmlspecialchars($rol['description']); ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <tr>
-                            <td colspan="2">No hay roles registrados.</td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</body>
+<div class="container">
+   <span class="txtC"> Consulta Roles</span>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre del Rol</th>
+                <th>Descripción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($roles)): ?>
+            <?php foreach ($roles as $rol): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($rol['name_rol']); ?></td>
+                <td><?php echo htmlspecialchars($rol['description']); ?></td>
+            </tr>
+            <?php endforeach; ?>
+            <?php else: ?>
+            <tr>
+                <td colspan="2">No hay roles registrados.</td>
+            </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</div>
+
 
 <?php require 'templates/footer.php'; ?>
