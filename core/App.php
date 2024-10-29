146e338fb4abd20ba30d8ebace041f4e0808a306
amirj <amirjhapargonzalezcamarena@gmail.com>
2024-10-29 13:09:55 -0500

    creacion de la vista de gestionEspecialidad

--- core/App.php
+++ core/App.php
@@ -27,9 +27,11 @@ class App {
             case 'login':
                 $controller->login(); // Cargar el método de inicio de sesión
                 break;
+
             case 'crearUsuario':
                 $controller->register(); // Cargar el método de registro
                 break;
+
             case 'admin':
                 // Verificar si el usuario está autenticado antes de mostrar el panel de administración
                 if (isset($_SESSION['user_id'])) {
@@ -38,40 +40,42 @@ class App {
                     require_once 'views/admin.php'; // Redirigir al inicio de sesión si no está autenticado
                 }
                 break;
+
             case 'logout':
                 $controller->logout(); // Cerrar sesión
                 break;
 
             case 'GestionUsuario':
-                // Si se accede a /users, se carga la vista de usuarios
-                    require_once 'views/GestionUsuario.php';
+                require_once 'views/GestionUsuario.php';
                 break;
 
             case 'createRoll':
-                // Si se accede a /rol, se carga la vista de roles
-                    require_once 'views/createRoll.php';
+                require_once 'views/createRoll.php';
                 break;
 
             case 'crearRol':
-                //Acceder a la vista registrar rol
-                    require_once 'views/crearRol.php';
+                require_once 'views/crearRol.php';
                 break;
             
             case 'registrarRol':
                 $rolcontroller->registrar_rol();
                 break;
+
             case 'consultarRoles':
                 $rolcontroller->consultar_roles();
                 break;
+
             case 'eliminarRol':
                 $rolcontroller->mostrar_roles_para_eliminar();
-
                 break;
 
             case 'consultarUsuario':
                 $controller->consultarUsuarios();
                 break;
-            
+
+            case 'GestionEspecialidad':
+                require_once 'views/GestionEspecialidad.php';
+                break;
 
             default:
                 echo "Página no encontrada"; // Mostrar error si la ruta no existe
